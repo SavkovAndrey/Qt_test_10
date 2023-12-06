e14564153a3aa9f88e3c52bcb7339983b44bea35
SavkovAndrey <141626360+SavkovAndrey@users.noreply.github.com>
2023-12-06 18:46:48 +0300

    тест меню

---
+++ mainwindow.cpp
@@ -0,0 +1,33 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_action_triggered()
+{
+    ui->statusbar->showMessage("Создание нового проекта");
+}
+
+
+void MainWindow::on_action_2_triggered()
+{
+      ui->statusbar->showMessage("Открытие нового проекта");
+}
+
+
+void MainWindow::on_action_3_triggered()
+{
+    QApplication::quit();
+}
+
